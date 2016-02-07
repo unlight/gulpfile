83575cc225f9413871e9f21575c45f63432c4478
Roman <s91630277@gmail.com>
2016-02-07 22:12:56 +0300

    Correct watchify

--- gulp/scripts.js
+++ gulp/scripts.js
@@ -1,33 +1,50 @@
 var config = require("./config");
+// var util = require("./util");
 var eslintp = require("./eslintp");
-var browserify = require("browserify");
+var source = require("vinyl-source-stream");
 var buffer = require("vinyl-buffer");
+var browserify = require("browserify");
+var watchify = require("watchify");
 
-gulp.task("scripts.browserify", function() {
-	var options = {
+function instance() {
+	var b = browserify(config.scripts.main, {
 		detectGlobals: false,
-		paths: ["src/scripts", "src", "node_modules", "./"],
+		// paths: ["src", "node_modules", "./"],
 		debug: config.debug,
 		// noParse: config.vendors
 		// transform: ["brfs", "folderify"]
-	};
-	var b = browserify(config.scripts.main, options);
+	});
 	config.vendors.forEach(function(v) {
 		b.external(v);
 	});
+	return b;
+}
+
+function bundle(b) {
 	return b.bundle()
+		.pipe(source("main.js"))
 		.pipe(buffer())
-		.pipe(g.sourcemaps.init({loadMaps: true}))
+		.pipe(g.sourcemaps.init({ loadMaps: true }))
 		.pipe(g.if(config.production, g.ngAnnotate()))
 		.pipe(g.if(config.production, g.uglify()))
 		.pipe(g.sourcemaps.write("./"))
 		.pipe(gulp.dest(config.scripts.dest))
 		.pipe(g.connect.reload());
+}
+
+gulp.task("scripts.browserify", function(done) {
+	var b = instance();
+	var stream = bundle(b);
+	stream.on("end", done);
 });
 
-gulp.task("scripts.watch", function() {
-	// TODO: use g.batch here
-	g.watch(config.scripts.watch, gulp.series("scripts.browserify"));
+gulp.task("scripts.watch", function(done) {
+	var b = watchify(instance());
+	bundle(b);
+	// On any dep update, runs the bundler
+	b.on("update", bundle.bind(null, b));
+	// Output build logs to terminal
+	b.on("log", g.util.log);
 });
 
 gulp.task("scripts.vendors", function() {
@@ -44,4 +61,3 @@ gulp.task("scripts.vendors", function() {
 		.pipe(g.sourcemaps.write("./"))
 		.pipe(gulp.dest(config.scripts.dest));
 });
-
